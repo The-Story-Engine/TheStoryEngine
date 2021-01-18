1c7d31d10a9a7ccd75fc02aba484eb75fe08995d
Jedd Fenner <jedd@outlook.com>
2021-01-18 11:46:12 +0000

    returning writer greeting, placeholder element for SSG page

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,37 @@
 import Layout from "@components/Layout";
 import Jellyfish from "@components/InkJellyfish";
 import LinkButton from "@components/LinkButton";
+import { useUserStory } from "utils-client";
+
+const Placeholder = () => <div className="w-full mt-16 h-60" />;
 
 export default function Home() {
+  const [story] = useUserStory();
+
+  const isStoryInitialized = !!story.id;
+  const hasStoryContent = !!(story.text || story.title);
   return (
     <Layout>
       <div className="flex flex-col items-center justify-center flex-grow p-4 lg:flex-row">
         <Jellyfish className="lg:mr-16 w-96" />
-        <div className="flex flex-col items-center max-w-sm mt-16 text-center lg:mt-0">
-          <h2 className="text-4xl font-bold text-grey-500">
-            Howdy! I'm
-            <br />
-            the Inspirational Jellyfish
-          </h2>
-          <p className="mt-6 text-2xl">
-            I've got lots of ideas to help with your writing, tap below to get
-            started!
-          </p>
-          <LinkButton href="/write">Write a story!</LinkButton>
+        <div className="flex flex-col items-center mt-16 text-center w-96 lg:mt-0">
+          {isStoryInitialized ? (
+            <>
+              <h2 className="text-4xl font-bold text-grey-500">
+                Howdy! I'm
+                <br />
+                the Inspirational Jellyfish
+              </h2>
+              <p className="mt-6 text-2xl">
+                {hasStoryContent
+                  ? "Welcome back, tap below to continue your story!"
+                  : "I've got lots of ideas to help with your writing, tap below to get started!"}
+              </p>
+              <LinkButton href="/write">Write a story!</LinkButton>
+            </>
+          ) : (
+            <Placeholder />
+          )}
         </div>
       </div>
     </Layout>
