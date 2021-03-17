d25a0b53934bf14882f32a3687b2c54e714102d9
Jedd Fenner <jedd@outlook.com>
2021-03-17 12:57:56 +0000

    disable backend reliant UI until AWS is sorted

--- pages/roadmap.js
+++ pages/roadmap.js
@@ -173,7 +173,7 @@ export default function WaitlistPage() {
     {
       title: "Writer Accounts",
       icon: WriterSVG,
-      description: "A space to iterate & save stories for writers of all ages.",
+      description: "A space to save & iterate stories for writers of all ages.",
     },
     {
       title: "Home Workspaces",
@@ -257,7 +257,7 @@ export default function WaitlistPage() {
                 )
               )}
             </div>
-            <div className="flex flex-col items-center pt-8">
+            {/* <div className="flex flex-col items-center pt-8">
               <h2
                 className="w-full font-semibold text-center text-h1"
                 id="waitlist"
@@ -298,13 +298,13 @@ export default function WaitlistPage() {
                   waitlist
                 )}
               </div>
-            </div>
-            <div className="flex flex-col items-center pt-8 space-y-12">
+            </div> */}
+            {/* <div className="flex flex-col items-center pt-8 space-y-12">
               <h2 className="w-full font-semibold text-center text-h1">
                 Donate
               </h2>
               <Donate />
-            </div>
+            </div> */}
           </div>
         </div>
       }
