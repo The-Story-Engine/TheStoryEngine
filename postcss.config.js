4290a2be485de0fe09fafa0cb5381e6a2c4f6994
Jedd Fenner <jedd@outlook.com>
2021-03-19 21:12:12 +0000

    lighthouse a11y fixes, update packages, switch to tailwind jit

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    "postcss-focus-visible": {},
+    "@tailwindcss/jit": {},
     autoprefixer: {},
+    "postcss-focus-visible": {},
   },
 };
